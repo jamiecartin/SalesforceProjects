82c537fb693ba286c52ab72667e44650afc6ef5b
Jamie Cartin <jamie.cartin@icloud.com>
2023-09-18 01:35:50 -0400

    Update HealthcareIntegration.java

--- HMS/HealthcareIntegration.java
+++ HMS/HealthcareIntegration.java
@@ -1,1 +1,31 @@
+import System.Http;
+import System.HttpRequest;
+import System.HttpResponse;
+import System.JSON;
 
+public class HealthcareIntegration {
+  //Methof to make REST API call to third party app
+  public static void makeAPIRequest(String endpoint, String requestBody) {
+    //create HTTP instance
+    Http http = new Http();
+
+    //create HTTP Req
+    HttpRequest request = new HttpRequest();
+    request.setEndpoint(endpoint);
+    request.setMethod('POST');
+    request.setHeader('Content-Type', 'application/json');
+    request.setBody(requestBody);
+
+    try {
+      //Send HTTP Req
+      HttpResponse response = http.send(request);
+
+      //process response
+      if (response.getStatusCode() == 200) {
+        //Successful response
+        String responseBody = response.getBody();
+        //Process the responseBody as needed
+      } else {
+        System.debug('Exception: ' + ex.getMessage());
+      }
+    }
